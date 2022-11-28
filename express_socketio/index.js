47753b54f5bf3f2db852738049568bc165f97639
HSUHAOBM <hao66bmbm@gmail.com>
2022-11-28 16:07:24 +0800

    feat: namespace, room, session, logout, broadcast
    express_socketio

--- express_socketio/index.js
+++ express_socketio/index.js
@@ -4,18 +4,101 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const port = 3310
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.set("view engine","ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const session = require("express-session");
+
+const sessionMiddleware = session({
+  secret: "changeit",
+  resave: false,
+  saveUninitialized: false
 });
+app.use(sessionMiddleware);
 
-io.on('connection', (socket) => {
+// middlewares
+const wrap = middleware => (
+  socket, next) => middleware(socket.request, {}, next);
+
+// 空間
+const chat_space = io.of("/chat");
+// add middlewares
+chat_space.use(wrap(sessionMiddleware));
+
+chat_space.on('connection', (socket) => {
+  let session = socket.request.session;
+  let user = session.user;
+  let room = session.room;
+
+  // 加房間
+  socket.join(room);
+
+  // 使用者加入
+  chat_space.to(room).emit('chat message', user + " join the room\r");
+
+  // 訊息傳送
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    chat_space.to(room).emit('chat message', user + " : " + msg );
+  });
+
+  // 登出聊天室按鈕
+  socket.on('left', () => {
+    console.log(session)
+    session.destroy(()=>{
+      chat_space.to(room).emit("chat message", user + " leave the room\n");
+      chat_space.to(session.id).disconnectSockets()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`disconnect: ${socket.id}`);
+  });
+
+  socket.on('broadcast', (msg) => {
+    console.log(msg);
+    chat_space.local.emit("chat message", user + ':' + msg + ' （在' + room + '房）\n');
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+// 首頁
+app.get("/",function(req,res){
+  let user = req.session.user;
+  let room = req.session.room;
+  if(user && room){
+    res.redirect("/chatroom")
+  }else{
+    res.render("room");
+  }
+});
+
+app.post("/chat",function(req,res){
+  // 接收表單
+  req.session.user = req.body.user;
+  req.session.room = req.body.room;
+  // create session
+  let user = req.session.user;
+  let room = req.session.room;
+
+  if(user && room){
+    res.redirect("/chatroom")
+  }
+})
+
+app.get("/chatroom",function(req,res){
+  let user = req.session.user;
+  let room = req.session.room;
+  if(user && room){
+    res.render('index', {room})
+  }else{
+    res.redirect("/")
+  }
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
